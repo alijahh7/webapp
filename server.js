b541e81e4ed2f6d69f539d39d3fa3dd26160598a
aakanksha-arun <desai.aaka@northeastern.edu>
2024-02-21 16:27:36 -0500

    add packer, service, systemd

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const {app} = require('./index');
+require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 
--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#Installing any updates
+#echo "Installing updates"
 #dnf update
 
-# Install Node.js
 echo "Installing Node.js..."
 sudo dnf module -y enable nodejs:20
 sudo dnf module -y install nodejs:20/common
 
-# Install and start PostgreSQL
 echo "Installing and starting PostgreSQL..."
 sudo dnf module list postgresql
 sudo dnf module enable -y postgresql:12
@@ -17,13 +15,10 @@ sudo postgresql-setup --initdb
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
-# Install unzip
-echo "Installing unzip..."
+echo "Installing unzip"
 sudo dnf -y install unzip
 
-# Edit pg_hba.conf for ident —> trust
-# Edit pg_hba.conf for ident —> trust
-echo "Editing pg_hba.conf..."
+echo "Editing pg_hba.conf"
 sudo sed -i 's/local   all             all                                     peer/local   all             all                                     trust/g' /var/lib/pgsql/data/pg_hba.conf
 sudo sed -i 's/host.*all.*all.*127\.0\.0\.1\/32.*ident/host    all             all             127.0.0.1\/32            trust/g' /var/lib/pgsql/data/pg_hba.conf
 sudo sed -i 's/host.*all.*all.*::1\/128.*ident/host    all             all             ::1\/128                 trust/g' /var/lib/pgsql/data/pg_hba.conf
@@ -31,8 +26,33 @@ sudo service postgresql restart
 cat /var/lib/pgsql/data/pg_hba.conf
 
 echo "Creating user and DB"
-sudo -u postgres psql -c "CREATE USER a WITH PASSWORD 'abc';"
-sudo -u postgres psql -c "CREATE DATABASE mydb;"
+sudo -u postgres psql -c "CREATE USER $db_user WITH PASSWORD '$db_pass';"
+sudo -u postgres psql -c "CREATE DATABASE $db_name;"
+
+echo "Creating group"
+sudo groupadd csye6225
+
+echo "Creating system user"
+sudo useradd -M -s /usr/sbin/nologin -g csye6225 csye6225
+id csye6225
+
+echo "Unzipping artifacts..."
+sudo unzip "/tmp/webapp.zip" -d /opt/
+cd /opt/webapp
+ls
+
+# Change ownership
+echo "Changing Ownership..."
+sudo chown -R csye6225:csye6225 "/opt/webapp/"
+#sudo chmod -R 755 /opt/webapp
+
+echo "Checking Ownership of webapp"
+ls -l "/opt/webapp/"
+
+# Install dependencies
+echo "Installing dependencies..."
+cd "/opt/webapp"
+sudo npm install
 
 echo "DONE!"
 

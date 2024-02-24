fec0fcc1c12f1429c672acfb8aba24da9676b724
aakanksha-arun <desai.aaka@northeastern.edu>
2024-02-24 01:24:10 -0500

    update script

--- helpers/database.js
+++ helpers/database.js
@@ -1,12 +1,14 @@
 require('dotenv').config();const {Sequelize}=require("sequelize");
 
-const db=process.env.PSQL_DB || 'mydb';
+const db=process.env.PSQL_DB;
 const user=process.env.PSQL_DB_USER;
 const pass=process.env.PSQL_DB_PASS;
+const host = process.env.PSQL_DB_HOST;
 const port = process.env.PORT || 8080;
 
 const sequelize = new Sequelize(db, user, pass,{
     dialect: 'postgres',
+    host: host,
     logging: false
 });
 
--- systemd.sh
+++ systemd.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+done_file="/tmp/startup.sh.done"
 
+check_startup_script_status() {
+    if [ -f "$done_file" ]; then
+        return 0 #file found
+    else
+        return 1 #file not found
+    fi
+}
+
+
+while ! check_startup_script_status; do
+    echo "Waiting for startup script to finish..."
+    sleep 10
+done
+
+echo "Startup Script Completed. Starting service..."
+
+#service commands
 sudo cp /opt/webapp/csye6225.service /etc/systemd/system/csye6225.service
 sudo systemctl daemon-reload
 sudo systemctl enable csye6225
